687a3514324471801ec5e62e90f016e66b2b3a9f
hiratara <hiratara@cpan.org>
2009-03-20 22:04:11 +0900

    initial commit

---
+++ lib/TAP/Formatter/RawData.pm
@@ -0,0 +1,33 @@
+package TAP::Formatter::RawData;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+TAP::Formatter::RawData -
+
+=head1 SYNOPSIS
+
+  use TAP::Formatter::RawData;
+
+=head1 DESCRIPTION
+
+TAP::Formatter::RawData is
+
+=head1 AUTHOR
+
+Masahiro Honma E<lt>hira.tara {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
